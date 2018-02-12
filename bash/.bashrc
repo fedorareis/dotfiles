df27a52f9c3e3d0c3df9256ffe42346d4dc2269a
fedorareis <fedorareis@gmail.com>
2018-02-11 23:46:55 -0800

    Fixed symlinking issue

--- bash/.bashrc
+++ bash/.bashrc
@@ -1,1 +1,39 @@
-/Users/kylereis/.bashrc
+#!/bin/bash
+
+source ~/.iterm2_shell_integration.bash
+
+export PATH=/usr/local/bin:$PATH
+
+export EIGEN3_INCLUDE_DIR=/usr/local/include/eigen3
+export GLFW_DIR=/usr/local/include/GLFW
+export GLEW_DIR=/usr/local/opt/glew
+
+#These lines set the bash coloring
+export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h: \[\033[33;1m\]\w\[\033[m\]\$ "
+export CLICOLOR=1
+export LSCOLORS=ExFxBxDxCxegedabagacad
+
+export TERM=xterm-color
+
+# Next set of lines is for ResDev
+export WORKON_HOME=$HOME/Documents/coding/ResDev/virtualenvs
+export PROJECT_HOME=$HOME/Documents/coding/ResDev/projects
+export DJANGO_SETTINGS_MODULE=settings.local
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
+export PYENV_ROOT=/usr/local/var/pyenv
+
+if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+
+pyenv virtualenvwrapper
+
+
+# Setting PATH for Python 3.4
+# The orginal version is saved in .bash_profile.pysave
+PATH="/Library/Frameworks/Python.framework/Versions/3.4/bin:${PATH}"
+export PATH
+
+
+#THIS MUST BE AT THE END OF THE FILE FOR GVM TO WORK!!!
+[[ -s "/Users/kylereis/.gvm/bin/gvm-init.sh" ]] && source "/Users/kylereis/.gvm/bin/gvm-init.sh"
--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,32 @@
-/Users/kylereis/.config/fish/config.fish
+set -g -x PATH /usr/local/bin $PATH
+set -g -x fish_greeting 'Welcome Kyle'
+
+set -x EIGEN3_INCLUDE_DIR /usr/local/include/eigen3
+set -x GLFW_DIR /usr/local/include/GLFW
+set -x GLEW_DIR /usr/local/opt/glew
+set -x ESSENTIA_INCLUDE_DIR /usr/local/include/essentia
+
+alias graphics "g++ -L/opt/local/lib -L/usr/local/lib -I/usr/local/include -lglfw3 -lglew -framework GLUT -framework OpenGL -v"
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+function envset --on-event virtualenv_did_activate:resnet_internal_project
+    set --export --global DJANGO_SETTINGS_MODULE settings.local
+end
+
+set --export --global WORKON_HOME $HOME/Documents/coding/ResDev/virtualenvs
+set --export --global PROJECT_HOME $HOME/Documents/coding/ResDev/projects
+set --export --global DJANGO_SETTINGS_MODULE settings.local
+#set --export --global PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV "true"
+#set --export --global PYENV_ROOT /usr/local/var/pyenv
+
+set --export --global VIRTUALFISH_HOME $WORKON_HOME
+eval (python3 -m virtualfish)
+
+# OpenSSL environment variables.
+set --export --global LDFLAGS  -L/usr/local/opt/openssl/lib
+set --export --global CPPFLAGS -I/usr/local/opt/openssl/include
+set --export --global PKG_CONFIG_PATH /usr/local/opt/openssl/lib/pkgconfig
+set -g fish_user_paths "/usr/local/opt/dirmngr/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/node@6/bin" $fish_user_paths
