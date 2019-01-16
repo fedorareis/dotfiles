3092efd32a5f217db32a6b62f390a0718329a9f8
fedorareis <fedorareis@gmail.com>
2019-01-15 23:37:33 -0800

    01-15-19 update

--- scripts/update.fish
+++ scripts/update.fish
@@ -1,4 +1,4 @@
 #!/usr/local/bin/fish
 
-brew upgrade; and brew cleanup; and brew prune; and fisher self-update; and fisher;
+brew upgrade; and brew cleanup; and fisher self-update; and fisher;
 brew bundle dump --force --file=../brew/Brewfile.full;
