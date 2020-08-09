c71f0a5ed091ccacf7aa578b22dd16c47ca5d7cc
fedorareis <fedorareis@gmail.com>
2020-08-09 09:32:09 -0700

    08-09-20 update

--- scripts/update.fish
+++ scripts/update.fish
@@ -1,4 +1,4 @@
 #!/usr/local/bin/fish
 
-brew upgrade; and brew cleanup; and fisher self-update; and fisher;
+brew upgrade; and brew cask upgrade --greedy; and brew cleanup; and fisher self-update; and fisher;
 brew bundle dump --force --file=../brew/Brewfile.full;
