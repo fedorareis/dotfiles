1feb336418accb3a758e97227df6d0308a290ec4
fedorareis <fedorareis@gmail.com>
2020-11-18 00:16:29 -0800

    update script for fisher 4.0

--- scripts/update.fish
+++ scripts/update.fish
@@ -1,4 +1,4 @@
 #!/usr/local/bin/fish
 
-brew upgrade; and brew upgrade --cask --greedy; and brew cleanup; and fisher self-update; and fisher;
+brew upgrade; and brew upgrade --cask --greedy; and brew cleanup; and fisher update;
 brew bundle dump --force --file=../brew/Brewfile.full;
