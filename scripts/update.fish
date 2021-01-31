ffa9360701f52d47e220931f5007b75550dc0a07
fedorareis <fedorareis@gmail.com>
2021-01-31 13:44:42 -0800

    01-31-21 update

--- scripts/update.fish
+++ scripts/update.fish
@@ -2,3 +2,4 @@
 
 brew upgrade; and brew upgrade --cask --greedy; and brew cleanup; and fisher update;
 brew bundle dump --force --file=../brew/Brewfile.full;
+pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U;
