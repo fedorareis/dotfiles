5d553b3dfdd8d0eb7c144620aea5d64e2bf5c243
fedorareis <fedorareis@gmail.com>
2023-09-20 23:36:20 -0700

    23-09-20 update

--- fish/functions/tide/configure/choices/powerline/powerline_prompt_heads.fish
+++ fish/functions/tide/configure/choices/powerline/powerline_prompt_heads.fish
@@ -6,19 +6,14 @@ function powerline_prompt_heads
     set -g fake_tide_right_prompt_prefix 
     _tide_display_prompt
 
-    _tide_option 2 Blurred
-    set -g fake_tide_left_prompt_suffix '▓▒░'
-    set -g fake_tide_right_prompt_prefix '░▒▓'
+    _tide_option 2 Slanted
+    set -g fake_tide_left_prompt_suffix 
+    set -g fake_tide_right_prompt_prefix 
     _tide_display_prompt
 
-    _tide_option 3 Slanted
-    set -g fake_tide_left_prompt_suffix ''
-    set -g fake_tide_right_prompt_prefix ''
-    _tide_display_prompt
-
-    _tide_option 4 Round
-    set -g fake_tide_left_prompt_suffix ''
-    set -g fake_tide_right_prompt_prefix ''
+    _tide_option 3 Round
+    set -g fake_tide_left_prompt_suffix 
+    set -g fake_tide_right_prompt_prefix 
     _tide_display_prompt
 
     _tide_menu (status function)
@@ -26,15 +21,12 @@ function powerline_prompt_heads
         case Sharp
             set -g fake_tide_left_prompt_suffix 
             set -g fake_tide_right_prompt_prefix 
-        case Blurred
-            set -g fake_tide_left_prompt_suffix '▓▒░'
-            set -g fake_tide_right_prompt_prefix '░▒▓'
         case Slanted
-            set -g fake_tide_left_prompt_suffix ''
-            set -g fake_tide_right_prompt_prefix ''
+            set -g fake_tide_left_prompt_suffix 
+            set -g fake_tide_right_prompt_prefix 
         case Round
-            set -g fake_tide_left_prompt_suffix ''
-            set -g fake_tide_right_prompt_prefix ''
+            set -g fake_tide_left_prompt_suffix 
+            set -g fake_tide_right_prompt_prefix 
     end
     _next_choice powerline/powerline_prompt_tails
 end
