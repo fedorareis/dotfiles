a43205898fa881a633b7a47ac08a53652702a063
fedorareis <fedorareis@gmail.com>
2023-12-01 22:09:56 -0800

    update 2023-12-02

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,6 @@
 set -g -x PATH /usr/local/bin $PATH
 set -g -x fish_greeting 'Welcome Kyle'
 
+alias ls="lsd"
+
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
