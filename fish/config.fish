58919940f90c96fb123da397ce03e98e8d6679c1
fedorareis <fedorareis@gmail.com>
2024-02-10 21:41:32 -0800

    update 2024-02-10

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,10 @@ set -g -x fish_greeting 'Welcome Kyle'
 alias ls="lsd"
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+# pnpm
+set -gx PNPM_HOME "/Users/kylereis/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
