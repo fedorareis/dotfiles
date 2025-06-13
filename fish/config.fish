7cd2ce55bbad4795f582246578a7b4c2a5f32085
fedorareis <fedorareis@gmail.com>
2025-06-12 23:10:32 -0700

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -20,9 +20,5 @@ if not string match -q -- $PNPM_HOME $PATH
 end
 # pnpm end
 
-# pyenv
-pyenv init - | source
-# pyenv end
-
 # Created by `pipx` on 2025-04-14 08:50:30
 set PATH $PATH /Users/kylereis/.local/bin
